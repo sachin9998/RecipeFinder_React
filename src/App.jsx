56c9c7876e5ee884ea6f92b0991e71bda271d36a
Sachin Alam <sachinalam4216@gmail.com>
2024-12-07 22:50:50 +0530

    ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import Sidebar from "./components/Sidebar";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,28 @@
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <>
+      <DesktopSidebar />
+      <MobileSidebar />
+    </>
+  );
 };
 
 export default Sidebar;
+
+const DesktopSidebar = () => {
+  return (
+    <div className="p-3 md:p-10 border-r min-h-screen w-24 md:w-64 hidden sm:block">
+      <div className="flex flex-col gap-20 sticky top-10 left-0">
+        <div className="w-full ">
+          <img src="./logo.svg" alt="Logo" className="hidden md:block" />
+          <img src="./mobile-logo.svg" alt="Logo" className="block md:hidden" />
+        </div>
+      </div>
+    </div>
+  );
+};
+const MobileSidebar = () => {
+  return (
+    <div className="p-3 md:p-10 border-r min-h-screen w-24 md:w-64 hidden sm:block"></div>
+  );
+};
