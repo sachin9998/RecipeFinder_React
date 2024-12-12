bb5d69fb6377e312eb4ec090cbbf89bc30df039c
Sachin Alam <sachinalam4216@gmail.com>
2024-12-12 18:29:02 +0530

    card designed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Search } from "lucide-react";
+import { Heart, HeartPulse, Search, Soup } from "lucide-react";
 
 const HomePage = () => {
   return (
@@ -9,13 +9,61 @@ const HomePage = () => {
             htmlFor=""
             className="input shadow-md flex items-center gap-2 "
           >
-            <Search size={24}/>
-            <input type="text" className="text-sm md:text-md grow" placeholder="What do you want to cook today?"/>
+            <Search size={24} />
+            <input
+              type="text"
+              className="text-sm md:text-md grow"
+              placeholder="What do you want to cook today?"
+            />
           </label>
         </form>
 
-        <p className="font-bold text"></p>
-        <p></p>
+        <h1 className="font-bold text-3xl md:text-5xl mt-4 ">
+          Recommended Recipes
+        </h1>
+
+        <p className="text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight">
+          Popular Choices
+        </p>
+
+        <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {/* <RecipeCard/> */}
+          <div className="flex flex-col rounded-md bg-[#ecf7d4] overflow-hidden p-3 relative">
+            <a href="#" className="relative h-32 ">
+              <img
+                className="rounded-md w-full h-full object-cover cursor-pointer"
+                src="./1.jpg"
+                alt="recipe image"
+              />
+
+              <div className="absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm">
+                <Soup size={16} /> 4 Serving
+              </div>
+
+              <div className="absolute top-1 right-2 bg-white rounded-full p-1 cursor-pointer">
+                <Heart
+                  size={20}
+                  className="hover:fill-red-500 hover:text-red-500"
+                />
+              </div>
+            </a>
+
+            <div className="flex mt-1">
+              <p className="font-bold tracking-wide">Roasted Chicken</p>
+            </div>
+
+            <p className="my-2 ">Turkish Kitchen</p>
+
+            <div className="flex gap-2 mt-auto">
+              <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
+                <HeartPulse size={16}/> <span className="text-sm tracking-tighter font-semibold">Gluten-free</span>
+              </div>
+              <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
+                <HeartPulse size={16}/> <span className="text-sm tracking-tighter font-semibold">Heart-healthy</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
